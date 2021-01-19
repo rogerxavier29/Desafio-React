abef3568e54491369650639ba4973e024739b73f
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-19 11:51:10 -0300

    add BrowserRouter, Routes
    add BrowserRouter, Routes para poder chamar Valor

--- vinhos/src/App.js
+++ vinhos/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Valor from './Valor';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="valor" element={<Valor />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
