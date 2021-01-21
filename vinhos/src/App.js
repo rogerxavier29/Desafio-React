756ca3c5de244b109a845936602dc9dfb57c8687
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-21 08:45:45 -0300

    import e Route
    importando as duas novas rotas de compra e cliente.

--- vinhos/src/App.js
+++ vinhos/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cliente from './Cliente';
+import Compra from './Compra';
 import Valor from './Valor';
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="valor" element={<Valor />} />
+        <Route path="compra" element={<Compra />} />
+        <Route path="cliente" element={<Cliente />} />
       </Routes>
     </BrowserRouter>
   );
