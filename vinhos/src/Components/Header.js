00f183d002d80b627237d5f0d1406b911501b389
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-21 12:16:33 -0300

    className
    colocando class name para poder manipular e estilzar no css

--- vinhos/src/Components/Header.js
+++ vinhos/src/Components/Header.js
@@ -5,10 +5,16 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav>
-        <Link to="valor">Ordem de Consumo</Link>
-        <Link to="compra">Clientes fieis.</Link>
-        <Link to="cliente">Cliente Master.</Link>
+      <nav className={styles.nav}>
+        <Link className={styles.ordem} to="valor">
+          Ordem de Consumo
+        </Link>
+        <Link className={styles.fieis} to="compra">
+          Clientes fieis.
+        </Link>
+        <Link className={styles.master} to="cliente">
+          Cliente Master.
+        </Link>
       </nav>
     </header>
   );
