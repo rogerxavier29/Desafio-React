333dabe17b7dbfce7a3ab2b33bb937266f6a22a0
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-21 10:47:05 -0300

    App  e App.css
    importando App.css para App e dando alguns resets na estilização

--- vinhos/src/App.js
+++ vinhos/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cliente from './Cliente';
 import Footer from './Components/Footer';
