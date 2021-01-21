833f03104119692df9494456ad73b0c197d7033a
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-21 10:35:46 -0300

    header e footer
    ja importado para pagina principal header e footer

--- vinhos/src/App.js
+++ vinhos/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cliente from './Cliente';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 import Compra from './Compra';
 import Valor from './Valor';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="valor" element={<Valor />} />
         <Route path="compra" element={<Compra />} />
         <Route path="cliente" element={<Cliente />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
