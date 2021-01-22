47a9275d0ef393d369985f0ebbf6dfe889f28c23
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-22 10:18:59 -0300

    compra
    acrescentado 3 div estrutura, estrutura2, estrutura3,  uma geral, uma que ira pegar nome do cliente  e outra que ira pegar os produtos do cliente

--- vinhos/src/Compra.js
+++ vinhos/src/Compra.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from './Compra.module.css';
 
 const Compra = () => {
   const [clienteTop, setClienteTop] = useState({});
@@ -19,24 +20,29 @@ const Compra = () => {
   }, []);
 
   return (
-    <div>
-      <li>
-        <p>Nome: {clienteTop && clienteTop.nome}</p>
-        <p>Valor: {clienteTop && clienteTop.valorTotal}</p>
-        <p>Data: {clienteTop && clienteTop.data}</p>
-        <p>Codigo: {clienteTop && clienteTop.codigo}</p>
-      </li>
-      <ul>
+    <div className={styles.estrutura}>
+      <h1>* Cliente - Master - 2016 *</h1>
+      <div className={styles.estrutura2}>
+        <li>
+          <p>Nome: {clienteTop && clienteTop.nome}</p>
+          <p>Valor: {clienteTop && clienteTop.valorTotal}</p>
+          <p>Data: {clienteTop && clienteTop.data}</p>
+          <p>Codigo: {clienteTop && clienteTop.codigo}</p>
+        </li>
+      </div>
+      <h2>Produtos - Adquiridos</h2>
+      <ul className={styles.ul}>
         {clienteTop.itens
           ? clienteTop.itens.map((item, index) => (
               <li key={item.codigo + index}>
-                <p>Produto: {item.produto} </p>
-                <p>Variedade: {item.variedade} </p>
-                <p>País: {item.pais} </p>
-                <p>Variedade: {item.variedade} </p>
-                <p>Categoria: {item.categoria} </p>
-                <p>Safra: {item.safra} </p>
-                <p>Preço: {item.preco} </p>
+                <div className={styles.estrutura3}>
+                  <p>Produto: {item.produto} </p>
+                  <p>Variedade: {item.variedade} </p>
+                  <p>País: {item.pais} </p>
+                  <p>Categoria: {item.categoria} </p>
+                  <p>Safra: {item.safra} </p>
+                  <p>Preço: {item.preco} </p>
+                </div>
               </li>
             ))
           : ''}
