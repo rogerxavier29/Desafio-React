07fe4868738edc499a776dbdb4a9227db811b3a8
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-26 17:05:02 -0300

    Recomendado.js
     useState select substituido para pegar pegarCliente
    criado um useState auxiliar clienteCpf, setClienteCpf, passando objeto vazio,
    depois criado um outro useState com objeto vazio pegarVinho, setPegarVinho.
    No useEffect do pegarCliente alterado o setSelect por setPegarCliente.
    
    getRecomendacao cpf, criando uma funcao onde foi passado o fetch para pegar os dados da api mencionada, onde foi passado um replace no cpf e um regex para retirar todos os caracteres do cpf.
    div: colocado uma className,
    depois criado um h1 colocado um className, criado um form com classname e dentro de form um select:  onde foi passado como value=clienteCpf, um onChange=setClienteCpf, para que o mesmo possa pegar o cliente e relacionar ao cpf, e getRecomendacao possa de acordo com seu cpf relacionar o vinho recomendado.
    pegarCliente passado um operador ternario  e map cpf, nome e option passado value e  key cpf do cliente.
    criado h3 mais classname e ul
    criado li's passando todos elementos que contem dentro do objeto pegarVinho

--- vinhos/src/Recomendado/Recomendado.js
+++ vinhos/src/Recomendado/Recomendado.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import Head from '../Components/Head';
+import styles from './Recomendado.module.css';
 
 const Recomendado = () => {
-  const [select, setSelect] = useState([]);
+  const [pegarCliente, setPegarCliente] = useState([]);
+  const [clienteCpf, setClienteCpf] = useState({});
   const URL_TO_FETCH = 'https://vendavinhos-backend.herokuapp.com/clients';
+
+  const [pegarVinho, setPegarVinho] = useState({});
 
   React.useEffect(() => {
     fetch(URL_TO_FETCH, {
@@ -11,7 +15,8 @@ const Recomendado = () => {
     })
       .then(function (response) {
         response.json().then(function (data) {
-          setSelect(data);
+          console.log(data);
+          setPegarCliente(data);
         });
       })
       .catch(function (err) {
@@ -19,22 +24,60 @@ const Recomendado = () => {
       });
   }, []);
 
+  const getRecomendacao = (cpf) => {
+    fetch(
+      `https://vendavinhos-backend.herokuapp.com/recommendWine/${cpf.replace(
+        /[^\d]+/g,
+        '',
+      )}`,
+      {
+        method: 'get', // opcional
+      },
+    )
+      .then(function (response) {
+        response.json().then(function (data) {
+          setPegarVinho(data);
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  };
+
   return (
-    <div>
+    <div className={styles.estrutura}>
       <Head
         title="Vinho Recomendado"
         description="Vinhos Recomendados com base no CPF"
       />
-      <select
-        value={select.cpf}
-        onChange={(cpf) => setSelect(select.target.value)}
-      >
-        {select.map((cpf, nome) => (
-          <option key={cpf} value={nome}>
-            {select.nome}
-          </option>
-        ))}
-      </select>
+      <h1 className={styles.h1title}>Recomendação</h1>
+      <form className={styles.form}>
+        <select
+          value={clienteCpf}
+          onChange={({ target }) => {
+            setClienteCpf(target.value);
+            getRecomendacao(target.value);
+          }}
+        >
+          {pegarCliente
+            ? pegarCliente.map(({ cpf, nome }) => (
+                <option id={cpf} value={cpf} key={cpf}>
+                  {nome}
+                </option>
+              ))
+            : ''}
+        </select>
+        <h3 className={styles.h3vinhos}>Vinho Recomendado :</h3>
+        <ul className={styles.vinhos}>
+          <li>Produto: {pegarVinho && pegarVinho.produto}</li>
+          <li>Variedade: {pegarVinho && pegarVinho.variedade}</li>
+          <li>País: {pegarVinho && pegarVinho.pais}</li>
+          <li>Categoria: {pegarVinho && pegarVinho.categoria}</li>
+          <li>Safra: {pegarVinho && pegarVinho.safra}</li>
+          <li>Preço: {pegarVinho && pegarVinho.preco}</li>
+          <li>Total: {pegarVinho && pegarVinho.total}</li>
+        </ul>
+      </form>
     </div>
   );
 };
